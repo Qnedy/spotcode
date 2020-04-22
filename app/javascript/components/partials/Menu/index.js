d4541387a223cc68afe848f45ec04c6ff145bbb0
qnedy <altmaisefequatro@gmail.com>
2020-04-21 21:13:44 -0300

    menu logout e login ok

--- app/javascript/components/partials/Menu/index.js
+++ app/javascript/components/partials/Menu/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Navbar, Container, Columns, Button } from 'react-bulma-components';
+import { Navbar, Container, Columns, Button, Dropdown } from 'react-bulma-components';
 import styled from 'styled-components';
 import logo from '../../../assets/images/logo.png';
 import { useLocation } from 'react-router-dom';
+import { FaUserCircle } from 'react-icons/fa';
 
 const ColumnsFullWidth = styled(Columns)`
     width: 100%;
@@ -10,6 +11,23 @@ const ColumnsFullWidth = styled(Columns)`
 `;
 
 const Menu = () => {
+  let actionButton;
+
+  useLocation().pathname == '/' ? actionButton = <a href="/users/sign_in" className="is-pulled-right is-right">
+    <Button outlined={true} color="white" >ENTRAR</Button>
+  </a> : actionButton = <Dropdown className='is-pulled-right is-right' color='dark' label={<FaUserCircle size="2em" />}>
+                          <Dropdown.Item value="other">
+                              <a href='/users/edit'>
+                                Edit User
+                              </a>
+                          </Dropdown.Item>
+                          <Dropdown.Item value="other">
+                              <a href='/users/sign_out'>
+                                LogOut
+                              </a>
+                          </Dropdown.Item>
+                        </Dropdown>;
+
   return(
     <>
       <Navbar color="dark">
@@ -19,9 +37,7 @@ const Menu = () => {
                     <img src={logo} className="image" />
                 </Columns.Column>
                 <Columns.Column>
-                  <a href="/users/sign_in" className="is-pulled-right is-right">
-                     <Button outlined={true} color="white" >ENTRAR</Button>
-                  </a>
+                  {actionButton}
                 </Columns.Column>
             </ColumnsFullWidth>
         </Container>
