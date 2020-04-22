61f37d54453249ae837d144027a196d6be01264a
qnedy <altmaisefequatro@gmail.com>
2020-04-21 21:39:33 -0300

    home ok

--- app/javascript/screen/home/index.js
+++ app/javascript/screen/home/index.js
@@ -1,25 +1,49 @@
 import React, { Fragment } from 'react';
-import { Columns, Heading } from 'react-bulma-components';
+import { Columns, Heading, Button } from 'react-bulma-components';
 import SectionWrapper from '../../components/partials/Section_wrapper';
+import styled from 'styled-components';
+
+const MainHeading = styled(Heading)`
+    margin-top: 50px;
+`;
+
+const DescriptionList = styled.ul`
+  margin-top: 50px;
+  font-size: 20px;
+  list-style-type: square;
+`
+
+const SubscribeButton = styled(Button)`
+    border-width: 2px;
+    margin-top: 50px;
+`;
  
 const HomeScreen = () => {
   return (
     <SectionWrapper>
       <Columns>
         <Columns.Column>
-            <Heading className='has-text-weight-light has-text-centered has-text-white' size={1}>
+            <MainHeading className='has-text-weight-light has-text-centered has-text-white' size={1}>
               SEU APP DE <br /><span className='has-text-success'>MÚSICA</span>
-            </Heading>
+            </MainHeading>
         </Columns.Column>
       </Columns>
       <Columns className="is-centered is-mobile">
         <Columns.Column mobile={{ size: 8, offset: 1 }} desktop={{ size: 4, offset: 2 }}>
-          
-         
+            <DescriptionList className='has-text-white'>
+                <li>Suas músicas <b>Favoritas</b></li>
+                <li>Seus <b>Podcasts</b></li>
+                <li>Os <b>Lançamentos</b></li>
+                <li>As novas <b>Descobertas</b></li>
+            </DescriptionList>
         </Columns.Column>
       </Columns>
       <Columns className="has-text-centered">
-       
+          <Columns.Column>
+            <a href="users/sign_up">
+                <SubscribeButton className="is-success is-outlined is-large has-text-white">CADASTRAR AGORA</SubscribeButton>
+            </a>
+          </Columns.Column>
       </Columns>
     </SectionWrapper>
   );
